3e0f9787ae053e328f8006744fdd96121ffebad8
Davi Duarte <davijduarte22@gmail.com>
2024-04-24 18:16:29 -0300

    Extracted cors origin to env variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { TransformInterceptor } from "./interceptors/transform.interceptors";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as process from "node:process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_ORIGIN,
     allowedHeaders: ["Content-Type", "Access-Control-Allow-Headers", "Authorization"]
   });
 
