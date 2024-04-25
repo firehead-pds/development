95c1c67e0b88ffebf01b1f7e45d4d15c29a28652
Davi Duarte <davijduarte22@gmail.com>
2024-04-24 21:03:05 -0300

    Re-added password hashing in user post route which was accidentally removed.

--- server/src/entities/users/users.service.ts
+++ server/src/entities/users/users.service.ts
@@ -4,12 +4,14 @@ import { User } from "./user.entity";
 import { UsersValidator } from "./validators/users.validator";
 import { InjectRepository } from "@nestjs/typeorm";
 import IUser from "./interfaces/IUser";
+import { HashingService } from "../../common/hashing/hashing.service";
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly repo: Repository<User>,
-    private readonly validator: UsersValidator
+    private readonly validator: UsersValidator,
+    private readonly hashingService: HashingService
   ) {
   }
 
@@ -18,6 +20,7 @@ export class UsersService {
   ) {
     await this.validator.validateCreateUser(user);
 
+    user.password = await this.hashingService.hashData(user.password);
     const newUser = this.repo.create(user);
     await this.repo.save(newUser);
 
