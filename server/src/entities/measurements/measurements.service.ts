4a95bcdee074dbaf9175dd483a04ef69e15bf69b
Davi Duarte <davijduarte22@gmail.com>
2024-05-02 17:33:06 -0300

    Removed unused code from address and measurements modules.

--- server/src/entities/measurements/measurements.service.ts
+++ server/src/entities/measurements/measurements.service.ts
@@ -1,55 +1,12 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Measurements } from "./measurements.entity";
-import { Repository } from "typeorm";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Measurements } from './measurements.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MeasurementsService {
   constructor(
     @InjectRepository(Measurements)
     private readonly repository: Repository<Measurements>,
-  ) {
-  }
-
-  public async create(body): Promise<Measurements> {
-    return this.repository.save(body);
-  }
-
-  public async findAll(): Promise<Measurements[]> {
-    return this.repository.find();
-  }
-
-  public async findOne(id: number): Promise<Measurements> {
-    const measurements = await this.repository.findOne({ where: { id } });
-
-    if (!measurements) {
-      throw new NotFoundException(`No measurements found.`);
-    }
-
-    return measurements;
-  }
-
-  public async update(id: number, body): Promise<Measurements> {
-    const measurements = await this.repository.findOne({ where: { id } });
-
-    if (!measurements) {
-      throw new NotFoundException(`No measurements found.`);
-    }
-
-    await this.repository.update({ id }, body);
-
-    return this.repository.findOne({ where: { id } });
-  }
-
-  public async delete(id: number): Promise<string> {
-    const user = await this.repository.findOne({ where: { id } });
-
-    if (!user) {
-      throw new NotFoundException(`No measurements found.`);
-    }
-
-    await this.repository.delete(id);
-
-    return "Measurements successfully deleted";
-  }
+  ) {}
 }
