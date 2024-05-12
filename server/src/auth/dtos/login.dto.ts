dc68ed3453c07b9e5a129cc89b617733a0587ec0
Davi Duarte <davijduarte22@gmail.com>
2024-05-12 00:34:57 -0300

    Added OpenAPI documentation to login route

--- server/src/auth/dtos/login.dto.ts
+++ server/src/auth/dtos/login.dto.ts
@@ -1,9 +1,12 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export default class LoginDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 }
