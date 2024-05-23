e60ef4290f980aad6040c88d7502c836f52db6d4
Davi Duarte <davijduarte22@gmail.com>
2024-05-23 00:12:12 -0300

    Removed unnecessary injections

--- server/src/entities/address/address.controller.ts
+++ server/src/entities/address/address.controller.ts
@@ -1,14 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Address } from './address.entity';
-import { AddressService } from './address.service';
 
 @Controller('/address')
 export class AddressController {
-  constructor(
-    @InjectRepository(Address)
-    private repository: Repository<Address>,
-    private readonly service: AddressService,
-  ) {}
+  constructor() {}
 }
--- server/src/entities/measurements/measurements.controller.ts
+++ server/src/entities/measurements/measurements.controller.ts
@@ -1,14 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Measurements } from './measurements.entity';
-import { MeasurementsService } from './measurements.service';
 
 @Controller('/measurements')
 export class MeasurementsController {
-  constructor(
-    @InjectRepository(Measurements)
-    private repository: Repository<Measurements>,
-    private readonly service: MeasurementsService,
-  ) {}
+  constructor() {}
 }
