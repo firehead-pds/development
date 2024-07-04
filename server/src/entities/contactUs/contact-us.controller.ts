94b406442b237cf537bd640ce1ff79419c097368
Igor Aguiar <aguiarigor365@gmail.com>
2024-07-04 12:58:46 -0300

    Fixed RTK-Query in ContactUs

--- server/src/entities/contactUs/contact-us.controller.ts
+++ server/src/entities/contactUs/contact-us.controller.ts
@@ -5,7 +5,9 @@ import {
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
 } from '@nestjs/swagger';
+import { Public } from '../../auth/decorators/is-public.decorator';
 
+@Public()
 @Controller('contact-us')
 export class ContactUsController {
   constructor(private readonly contactUsService: ContactUsService) {}
@@ -19,6 +21,6 @@ export class ContactUsController {
   async sendEmail(@Body() emailData: SendMessageDto) {
     const { userEmail, title, message } = emailData;
     await this.contactUsService.sendContactEmail(userEmail, title, message);
-    return 'message sent successfully';
+    return { message: 'message sent successfully' };
   }
 }
