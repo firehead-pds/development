6aac0a0db90db44233dbf6ce985fab15ca3dc044
Davi Duarte <davijduarte22@gmail.com>
2024-07-08 18:16:03 -0300

    Configured typeorm to run migrations

--- server/src/config/database.module.ts
+++ server/src/config/database.module.ts
@@ -7,24 +7,13 @@ import { Measurements } from '../entities/measurements/measurements.entity';
 import * as process from 'node:process';
 import Token from '../auth/token.entity';
 import { Friendship } from '../entities/friendships/friendship.entity';
+import { dataSourceOptions } from '../../typeorm.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: +config.get('DB_PORT'),
-        database: config.get('DB_DATABASE'),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        synchronize: process.env.NODE_ENV !== 'production',
-        dropSchema: process.env.NODE_ENV === 'test',
-        autoLoadEntities: true,
-        entities: [User, Address, Measurements, Token, Friendship],
-        ssl: true,
-      }),
+      useFactory: (config: ConfigService) => dataSourceOptions,
     }),
   ],
 })
