f9ef3f73c683ff1c63d604749cced1b310afad79
Davi Duarte <davijduarte22@gmail.com>
2024-07-08 18:17:09 -0300

    Remove unnecessary imports

--- server/src/config/database.module.ts
+++ server/src/config/database.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
-import { User } from '../entities/users/user.entity';
-import { Address } from '../entities/address/address.entity';
-import { Measurements } from '../entities/measurements/measurements.entity';
-import * as process from 'node:process';
-import Token from '../auth/token.entity';
-import { Friendship } from '../entities/friendships/friendship.entity';
 import { dataSourceOptions } from '../../typeorm.config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => dataSourceOptions,
+      useFactory: () => dataSourceOptions,
     }),
   ],
 })
