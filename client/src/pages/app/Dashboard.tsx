b4e4466ae1cb24a03e03aa82a8611d08c55f3994
Igor Aguiar <aguiarigor365@gmail.com>
2024-07-19 17:47:31 -0300

    Move wing creation and added all user wings list into dashboard

--- client/src/pages/app/Dashboard.tsx
+++ client/src/pages/app/Dashboard.tsx
@@ -1,18 +1,64 @@
 import { useAppSelector } from '../../app/hook.ts';
 import { selectCurrentUser } from '../../features/auth/authSlice.ts';
 import { Link as ReactRouterLink } from 'react-router-dom';
-import { Link } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Link,
+} from '@chakra-ui/react';
+import { useCreateWingMutation } from '../../features/wing/wingApiSlice.ts';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+interface CreateWingFormFields {
+  wingName: string;
+}
 
 export default function Dashboard() {
   const user = useAppSelector(selectCurrentUser);
+  const [wing, { isLoading }] = useCreateWingMutation();
+
+  const { register, handleSubmit } = useForm<CreateWingFormFields>();
+  const onSubmit: SubmitHandler<CreateWingFormFields> = async (data) => {
+    try {
+      await wing(data).unwrap();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-      {user?.firstName}
-
-      <Link as={ReactRouterLink} to={'/app/wing'}>
-        Wings Storage
-      </Link>
+      {user?.firstName + ' ' + user?.lastName}
+      <br />
+      <br />
+      <form
+        id={'createWingForm'}
+        onSubmit={handleSubmit(onSubmit)}
+        className={'p-4 border rounded-lg mx-2 w-[500px]'}
+        noValidate
+      >
+        <FormControl isRequired>
+          <FormLabel htmlFor="wingName">Wing Name:</FormLabel>
+          <Input id="wingName" type="text" {...register('wingName')}></Input>
+        </FormControl>
+        <Button isLoading={isLoading} type={'submit'} form={'createWingForm'}>
+          Create
+        </Button>
+      </form>
+      <br />
+      {user?.wingsInfo?.map((wing) => {
+        return (
+          <Box className={'p-4 border rounded-lg mx-2 w-[500px]'}>
+            <Link as={ReactRouterLink} to={`/app/wing/${wing.id}`}>
+              {wing.id}
+            </Link>
+            {wing.userRole}
+          </Box>
+        );
+      })}
     </>
   );
 }
